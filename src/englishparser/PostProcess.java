d2212f3dfd2b617c9647544f33012a384e48dbe9
Peng CHENG <haidaoxiaofei@gmail.com>
2014-08-23 11:54:21 +0800

    postProcess

---
+++ src/englishparser/PostProcess.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package englishparser;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author bigstone
+ */
+public class PostProcess {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "/home/bigstone/Downloads/computer_NP.txt";
+        String outPath = "/home/bigstone/Downloads/computer_NP_OUT.txt";
+        String stopWords = "/home/bigstone/Documents/MATLAB/stopWordList";
+        List<String> stopWordsList = new ArrayList<String>();
+        
+        FileWriter fw = new FileWriter(outPath);
+
+        InputStream fis;
+        BufferedReader br;
+        String line;
+
+        fis = new FileInputStream(stopWords);
+        br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+        while ((line = br.readLine()) != null) {
+            stopWordsList.add(line);
+        }
+
+        br.close();
+        br = null;
+        fis = null;
+        
+        
+        fis = new FileInputStream(filePath);
+        br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+        while ((line = br.readLine()) != null) {
+            line = line.trim();
+            String[] words = line.split(" ");
+            
+            for (int i = 0; i < words.length; i++) {
+                boolean stayFlag = true;
+                for (String stopword : stopWordsList) {
+                    if(stopword.equals(words[i])){
+                        stayFlag = false;
+                        break;
+                    }
+                }
+                
+                if(stayFlag == true){
+                    fw.write(words[i]);
+                    fw.write(" ");
+                }
+            }
+            
+            fw.write("\n");
+        }
+
+        fw.flush();
+        fw.close();
+        br.close();
+        br = null;
+        fis = null;
+    }
+}
