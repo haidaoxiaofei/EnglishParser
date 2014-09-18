ce81a1b524eb1fd65dff1860a6bc40ae31f9bee1
bigstone <haidaoxiaofei@gmail.com>
2014-09-19 00:04:17 +0800

    save cpu4

--- src/englishparser/PostProcess.java
+++ src/englishparser/PostProcess.java
@@ -22,11 +22,15 @@ import java.util.List;
 public class PostProcess {
 
     public static void main(String[] args) throws IOException {
-        String filePath = "/home/bigstone/Downloads/computer_NP.txt";
-        String outPath = "/home/bigstone/Downloads/computer_NP_OUT.txt";
-        String stopWords = "/home/bigstone/Documents/MATLAB/stopWordList";
-        List<String> stopWordsList = new ArrayList<String>();
+//        String filePath = "/home/bigstone/Documents/computer_NP_REFINE.txt";
+//        String outPath = "/home/bigstone/Documents/computer_NP_OUT.txt";
+//        String stopWords = "/home/bigstone/Documents/stopWordsList";
         
+        String filePath = "/home/bigstone/tmp/allNP_less.txt";
+        String outPath = "/home/bigstone/tmp/allNP_less_refined.txt";
+        String stopWords = "/home/bigstone/tmp/stopWordsList";
+        List<String> stopWordsList = new ArrayList<String>();
+
         FileWriter fw = new FileWriter(outPath);
 
         InputStream fis;
@@ -42,44 +46,49 @@ public class PostProcess {
         br.close();
         br = null;
         fis = null;
-        
+
         List<String> wordsList = new ArrayList();
-        
+
         fis = new FileInputStream(filePath);
         br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+        int count = 0;
         while ((line = br.readLine()) != null) {
+            if (count++ % 1000 == 0) {
+                System.out.println(count/1000);
+            }
             line = line.trim();
             String[] words = line.split(" ");
-            
+
             for (int i = 0; i < words.length; i++) {
                 boolean stayFlag = true;
+
+                if (words[i].startsWith(">")||words[i].startsWith("?")||words[i].startsWith("v6")||words[i].startsWith("v5") ||words[i].startsWith("v4")||words[i].startsWith("v3")||words[i].startsWith("v2")||words[i].startsWith("v1")||words[i].startsWith("a0")||words[i].startsWith("a9")||words[i].startsWith("a8")||words[i].startsWith("a7")||words[i].startsWith("\\")||words[i].contains(".")||words[i].contains("#")||words[i].startsWith("isbn")||words[i].startsWith(":")||words[i].startsWith(";")||words[i].startsWith("$")||words[i].startsWith("`")||words[i].startsWith(",")||words[i].startsWith("\'")||words[i].startsWith("h2")||words[i].startsWith("a6")||words[i].startsWith("a5")||words[i].startsWith("a3")||words[i].startsWith("a2")||words[i].startsWith("c6")||words[i].startsWith("c5")||words[i].startsWith("c4")||words[i].startsWith("c2")||words[i].startsWith("c3")||words[i].startsWith("c1")||words[i].startsWith("a1")||words[i].startsWith("~")||words[i].startsWith("+")||words[i].startsWith("<")||words[i].startsWith(".")||words[i].startsWith("-")||words[i].startsWith("&")||words[i].startsWith("$") || words[i].length() == 0 || Character.isDigit(words[i].charAt(0)) || words[i].startsWith("%")) {
+                    stayFlag = false;
+                    continue;
+                }
                 for (String stopword : stopWordsList) {
-                    if(stopword.equals(words[i])){
-                        stayFlag = false;
-                        break;
-                    }
-                    
-                    if(words[i].startsWith("$") || Character.isDigit(words[i].charAt(0)) || words[i].startsWith("%")){
+                    if (stopword.equals(words[i])) {
                         stayFlag = false;
                         break;
                     }
                 }
-                
-                if(stayFlag == true){
+
+                if (stayFlag == true) {
                     wordsList.add(words[i]);
                 }
             }
-            
-            if(wordsList.size() >= 2){
-                for(String s : wordsList){
+
+            if (wordsList.size() >= 1) {
+                for (String s : wordsList) {
                     fw.write(s);
                     fw.write(" ");
                 }
-                
+
                 fw.write("\n");
             }
             
-            
+            wordsList.clear();
+
         }
 
         fw.flush();
